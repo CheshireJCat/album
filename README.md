aa7184395b60b54a204344288e462cc5811b8bae
jinye <jinye@talkweb.com.cn>
2017-10-12 18:27:02 +0800

    增加自动修改文件名工具

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # album
 相册
+
+[上传工具](https://github.com/qiniu/QSunSync?ref=developer.qiniu.com)
+[相册地址](http://lmaomaoz.com/album/)
---
+++ cname.js
@@ -0,0 +1,41 @@
+// 用于序列化重命名图片
+var fs = require("fs");
+var path = require("path");
+var args = process.argv.splice(2);
+var fileDirectory = args[0];
+var re = /\.(jpg|png|gif|jpeg)$/i;
+
+if (fs.existsSync(fileDirectory)) {
+    var files = fs.readdirSync(fileDirectory);
+    var l = files.length;
+    var res = 0;
+    var img = 0;
+    var notImg = 0;
+    files.forEach(function(file,i) {
+        var filePath = path.join(fileDirectory,file);
+        if (re.test(file)) {
+            var fileName = i + file.match(re)[0];
+            var newFilePath = path.join(fileDirectory, fileName);
+
+            fs.rename(filePath, newFilePath, function(err) {
+                if (err) throw err;
+                res++;
+                img++;
+                complete();
+                // console.log(filePath + ' ok~');
+            });
+        }else{
+            res++;
+            notImg++;
+            complete();
+            // console.log(file +' is not a image');
+        }
+    });
+    function complete(){
+        if(res == l){
+            console.log(img + ' images has been rename, ' + notImg + ' files is not image.');
+        }
+    }
+}else{
+    console.log(fileDirectory +' is null.');
+}
